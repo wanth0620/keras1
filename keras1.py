ce269f1c7897cd627e4f085aab11f9d67b9d8982
WanTingHsu <new003hoom003@gmail.com>
2018-07-03 09:55:04 +0800

    lose function ,optimization, training step

--- keras1.py
+++ keras1.py
@@ -23,4 +23,24 @@ model = Sequential()
 model.add(Dence(output_dim = 1,input_dim = 1))
 
 #Choose lose function and optimizing method
+model.compile(loss='mse', optimizer='sgd')
 
+# training
+print('Training -----------')
+for step in range(301):
+    cost = model.train_on_batch(X_train, Y_train)
+    if step % 100 == 0:
+        print('train cost: ', cost)
+
+# test
+print('\nTesting ------------')
+cost = model.evaluate(X_test, Y_test, batch_size=40)
+print('test cost:', cost)
+W, b = model.layers[0].get_weights()
+print('Weights=', W, '\nbiases=', b)
+
+# plotting the prediction
+Y_pred = model.predict(X_test)
+plt.scatter(X_test, Y_test)
+plt.plot(X_test, Y_pred)
+plt.show()
